abca3046b887d133f6bc32de9ac34c0664f54517
phuongda <phuongdaps03397@gmail.com>
2021-11-27 22:40:47 +0700

    Lession #21: Ket Noi Toi MySQL Database voi Sequelize - Render Du Lieu Ra View

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,6 +1,13 @@
-let getHomePage = (req, rep) => {
-    // return rep.send('Xin chào mọi người!');
-    return rep.render('homepage.ejs');
+import db from '../models/index';
+
+let getHomePage = async (req, rep) => {
+    try {
+        let data = await db.User.findAll();
+        // return rep.send('Xin chào mọi người!');
+        return rep.render('homepage.ejs', { data: JSON.stringify(data) });
+    } catch (error) {
+        console.log(error);
+    }
 }
 
 let getAboutPage = (req, rep) => {
--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import viewEngine from './config/viewEngine';
 import initWebRouters from './route/web';
+import connectDB from './config/connectDB';
 require('dotenv').config()
 
 let app = express();
@@ -13,6 +14,8 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 viewEngine(app);
 initWebRouters(app);
+
+connectDB();
 
 let port = process.env.PORT || 6969;
 //port === undefined => port 6969
